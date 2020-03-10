558f7465ecc51cffb264404c703ccaced7e917f5
drmanasa18.rao@gmail.com <drmanasa18.rao@gmail.com>
2020-03-10 14:35:02 +0100

    adds logic for handleClick

--- src/App.js
+++ src/App.js
@@ -7,19 +7,44 @@ class App extends React.Component {
       result:null
     }
   }
-  handleClick(){
-
+  handleClick(sel){
+    let userChoice = sel
+    let temp = ["rock","paper","scissor"]
+    let randomNum = Math.floor(Math.random() * 3);
+    let computerChoice = temp[randomNum]
+    if (userChoice === computerChoice){
+      console.log("Tie!");
+    }
+    else if (userChoice === 'paper' && computerChoice === 'rock'){
+      console.log("You win!");
+    }
+    else if (userChoice === 'rock' && computerChoice === 'scissors'){
+      console.log("You win!");
+    }
+    else if (userChoice === 'scissors' && computerChoice === 'rock'){
+      console.log("You lose!");
+    }
+    else if (userChoice === 'rock' && computerChoice === 'paper'){
+      console.log("You lose!");
+    }
+    else if (userChoice === 'paper' && computerChoice === 'scissors'){
+      console.log("You lose!");
+    }
+    else if (userChoice === 'scissors' || computerChoice === 'paper'){
+    console.log("You win!");
+    }
+    console.log(`User choice is ${userChoice} & Computer Choice is ${computerChoice}`)
   }
-render(){
-  return(
-<div>
-  <button>Rock</button>
-  <button>Paper</button>
-  <button>Scissor</button>
-</div>
-  )
- }
-
+  render() {
+    return(
+      <div>
+        <button onClick={() =>this.handleClick("rock")}>Rock</button>
+        <button onClick={() =>this.handleClick("paper")}>Paper</button>
+        <button onClick={() =>this.handleClick("scissor")}>Scissor</button>
+        <p>{this.state.result}</p>
+      </div>
+    )
+  } 
 }
 
 export default App;
